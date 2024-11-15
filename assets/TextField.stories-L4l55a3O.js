4577cc59044f80c3e2c236b7007acb2f849ac704
M Saad Maqsood <72228197+MSaadMaqsood@users.noreply.github.com>
2024-11-15 18:59:16 +0500

    Updates

---
+++ assets/TextField.stories-L4l55a3O.js
@@ -0,0 +1,20 @@
+import{j as s}from"./jsx-runtime-CkxqCPlQ.js";import{T as a}from"./TextField-B6oFgrwe.js";import{f as n}from"./index-DQLiMaGX.js";import{F as y}from"./index-DDFlhD8g.js";import"./index-DJO9vBfz.js";import"./clsx-B-dksMZM.js";/* empty css              */const H={component:a,tags:["autodocs"],parameters:{layout:"centered"},argTypes:{label:{control:{type:"text"}},required:{control:{type:"boolean"}},placeHolder:{control:{type:"text"}},value:{control:{type:"text"}},size:{options:["450px","100%"],control:{type:"select"}},minHeight:{options:["81px","max-content"],control:{type:"select"}},variant:{options:["text","number","password"],control:{type:"select"}},disabled:{control:{type:"boolean"}},errorMsg:{control:{type:"text"}}},args:{onChange:n(),onIconclick:n(),label:"Hi"}},e={args:{},render:r=>s.jsx(a,{...r})},o={args:{errorMsg:"This can't be empty."},render:r=>s.jsx(a,{...r})},t={args:{icon:s.jsx(y,{color:"#fff"})},render:r=>s.jsx(a,{...r})};var c,p,i;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {},
+  render: args => {
+    return <TextField {...args} />;
+  }
+}`,...(i=(p=e.parameters)==null?void 0:p.docs)==null?void 0:i.source}}};var m,l,d;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    errorMsg: "This can't be empty."
+  },
+  render: args => {
+    return <TextField {...args} />;
+  }
+}`,...(d=(l=o.parameters)==null?void 0:l.docs)==null?void 0:d.source}}};var g,u,x;t.parameters={...t.parameters,docs:{...(g=t.parameters)==null?void 0:g.docs,source:{originalSource:`{
+  args: {
+    icon: <FaBeer color="#fff" />
+  },
+  render: args => {
+    return <TextField {...args} />;
+  }
+}`,...(x=(u=t.parameters)==null?void 0:u.docs)==null?void 0:x.source}}};const I=["Primary","Error","WithIcon"];export{o as Error,e as Primary,t as WithIcon,I as __namedExportsOrder,H as default};
