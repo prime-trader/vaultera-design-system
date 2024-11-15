ae5dd8f8d7b3f57691b466b9c0249c78077f7a48
M Saad Maqsood <72228197+MSaadMaqsood@users.noreply.github.com>
2024-11-15 18:39:25 +0500

    Updates

---
+++ assets/Button.stories-Lmg4busF.js
@@ -0,0 +1,27 @@
+import{j as e}from"./jsx-runtime-CkxqCPlQ.js";import{B as t}from"./Button-DZajqa2P.js";import{f}from"./index-DQLiMaGX.js";import{F as j}from"./index-DDFlhD8g.js";import"./index-DJO9vBfz.js";import"./clsx-B-dksMZM.js";/* empty css              */const I={component:t,tags:["autodocs"],parameters:{layout:"centered"},argTypes:{label:{control:{type:"text"}},size:{options:["150px","100%"],control:{type:"select"}},variant:{options:["primary","outline"],control:{type:"select"}},disabled:{control:{type:"boolean"}},type:{options:["button","submit"],control:{type:"select"}}},args:{onClick:f(),label:"Hi"}},s={args:{},render:r=>e.jsx(t,{...r})},a={args:{variant:"outline"},render:r=>e.jsx(t,{...r})},n={args:{icon:e.jsx(j,{})},render:r=>e.jsx(t,{...r})},o={args:{disabled:!0},render:r=>e.jsx(t,{...r})};var c,i,p;s.parameters={...s.parameters,docs:{...(c=s.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {},
+  render: args => {
+    return <Button {...args} />;
+  }
+}`,...(p=(i=s.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};var u,m,d;a.parameters={...a.parameters,docs:{...(u=a.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    variant: "outline"
+  },
+  render: args => {
+    return <Button {...args} />;
+  }
+}`,...(d=(m=a.parameters)==null?void 0:m.docs)==null?void 0:d.source}}};var l,g,x;n.parameters={...n.parameters,docs:{...(l=n.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    icon: <FaBeer />
+  },
+  render: args => {
+    return <Button {...args} />;
+  }
+}`,...(x=(g=n.parameters)==null?void 0:g.docs)==null?void 0:x.source}}};var y,b,B;o.parameters={...o.parameters,docs:{...(y=o.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    disabled: true
+  },
+  render: args => {
+    return <Button {...args} />;
+  }
+}`,...(B=(b=o.parameters)==null?void 0:b.docs)==null?void 0:B.source}}};const P=["Primary","Outline","WithIcon","Disabled"];export{o as Disabled,a as Outline,s as Primary,n as WithIcon,P as __namedExportsOrder,I as default};
