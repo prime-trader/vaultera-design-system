c406154d4e270bf83e38c47d9fa11ec22227a813
Saad <msaadmaqsood.official@gmail.com>
2024-11-20 19:21:13 +0500

    Updates

---
+++ assets/FollowingStatus.stories-C17vXY2R.js
@@ -0,0 +1,6 @@
+import{j as r}from"./jsx-runtime-CkxqCPlQ.js";import"./index-DJO9vBfz.js";const i="_FollowingStatus_y90w7_1",s={FollowingStatus:i},o=e=>r.jsx("div",{className:`${s.FollowingStatus} ${s[e.varient]}`,children:r.jsx("label",{children:e.varient=="following"?"Following":e.varient=="resend"?"Resend":"Not Following"})});o.__docgenInfo={description:"",methods:[],displayName:"FollowingStatus",props:{varient:{required:!0,tsType:{name:"string"},description:""}}};const g={component:o,tags:["autodocs"],parameters:{layout:"centered"},argTypes:{label:{control:{type:"text"}}},args:{}},t={args:{},render:e=>r.jsx(o,{...e})};var n,a,l;t.parameters={...t.parameters,docs:{...(n=t.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {},
+  render: args => {
+    return <FollowingStatus {...args} />;
+  }
+}`,...(l=(a=t.parameters)==null?void 0:a.docs)==null?void 0:l.source}}};const u=["Primary"];export{t as Primary,u as __namedExportsOrder,g as default};
