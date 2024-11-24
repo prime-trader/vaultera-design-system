0f9ddfce8f527c640369bc9491576b95459d508b
M Saad Maqsood <72228197+MSaadMaqsood@users.noreply.github.com>
2024-11-24 18:40:53 +0500

    Updates

---
+++ assets/FollowingStatus.stories-Dgm8EDCo.js
@@ -0,0 +1,6 @@
+import{j as r}from"./jsx-runtime-CkxqCPlQ.js";/* empty css              */import"./index-DJO9vBfz.js";const i="_FollowingStatus_1vm9d_1",d="_following_1vm9d_26",c="_resend_1vm9d_37",n={FollowingStatus:i,following:d,resend:c},t=o=>r.jsx("div",{className:`${n.FollowingStatus} ${n[o.varient]}`,children:r.jsx("label",{children:o.varient=="following"?"Following":o.varient=="resend"?"Resend":"Not Following"})});t.__docgenInfo={description:"",methods:[],displayName:"FollowingStatus",props:{varient:{required:!0,tsType:{name:"string"},description:""}}};const p={component:t,tags:["autodocs"],parameters:{layout:"centered"},argTypes:{varient:{options:["following","resend","not-following"],control:{type:"select"}}},args:{}},e={args:{},render:o=>r.jsx(t,{...o})};var s,a,l;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {},
+  render: args => {
+    return <FollowingStatus {...args} />;
+  }
+}`,...(l=(a=e.parameters)==null?void 0:a.docs)==null?void 0:l.source}}};const w=["Primary"];export{e as Primary,w as __namedExportsOrder,p as default};
