61b330239891cb6a95913b5b1e5825982c0aa246
Saad <msaadmaqsood.official@gmail.com>
2024-11-25 21:38:16 +0500

    Updates

---
+++ assets/LoginSocialBtn.stories-Ahg7AA_0.js
@@ -0,0 +1,6 @@
+import{j as e}from"./jsx-runtime-CkxqCPlQ.js";/* empty css              */import"./index-DJO9vBfz.js";const i={},o=n=>e.jsx("div",{className:`${i.LoginSocialBtn}`,children:e.jsx("h1",{children:"Testing"})});o.__docgenInfo={description:"",methods:[],displayName:"LoginSocialBtn"};const l={component:o,tags:["autodocs"],parameters:{layout:"centered"},argTypes:{varient:{options:["following","resend","not-following"],control:{type:"select"}}},args:{}},r={args:{},render:n=>e.jsx(o,{...n})};var s,t,a;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {},
+  render: args => {
+    return <LoginSocialBtn {...args} />;
+  }
+}`,...(a=(t=r.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const m=["Primary"];export{r as Primary,m as __namedExportsOrder,l as default};
