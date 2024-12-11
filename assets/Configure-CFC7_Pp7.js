1508c2e138c1baa179b320038e40ab3a13fcf7ef
raoasifraza11 <raoasifraza1@hotmail.com>
2024-12-11 20:01:38 +0500

    Updates

---
+++ assets/Configure-CFC7_Pp7.js
@@ -0,0 +1,16 @@
+import{j as t}from"./jsx-runtime-OAISXtky.js";import{useMDXComponents as l}from"./index-DOvxcyG1.js";import{ag as n,ah as o,ai as e}from"./chunk-NUUEMKO5-9EQiJhmL.js";import"./index-qVDZzPEr.js";import"./iframe-B-m7GN3O.js";import"../sb-preview/runtime.js";import"./index-ClewgdaX.js";import"./index-D2o5vtt7.js";import"./index-DrFu-skq.js";import"./react-18-C40XRLr0.js";function i(s){const r={h1:"h1",h2:"h2",...l(),...s.components};return t.jsxs(t.Fragment,{children:[t.jsx(n,{title:"Vaultra Design System/Color & Styles"}),`
+`,t.jsx(r.h1,{id:"color--styles",children:"Color & Styles"}),`
+`,t.jsx("br",{}),`
+`,t.jsx(r.h2,{id:"system--cta",children:"System & CTA"}),`
+`,t.jsx("br",{}),`
+`,t.jsxs(o,{children:[t.jsx(e,{title:"System",subtitle:"",colors:{LightGrey_Bkg:"#D6DADE",Header_Menu_Bkg:"#040F31",Screen_Bkg:"#FFFFFF",PrimaryFont:"#4B4B4B"}}),t.jsx(e,{title:"Primary",subtitle:"",colors:{Default:"#3491FF",Hover:"#3585E5",PressBorder__HoverBkg:"#DAEEFF"}}),t.jsx(e,{title:"Disabled CTA",subtitle:"",colors:{Disabled_CTA:"#C4C4C4"}}),t.jsx(e,{title:"Save CTA",subtitle:"",colors:{Save_CTA:"#47B576",Save_CTA_hover:"#3EA662"}})]}),`
+`,t.jsx("br",{}),`
+`,t.jsx(r.h2,{id:"traffic-light-system",children:"Traffic Light System"}),`
+`,t.jsx("br",{}),`
+`,t.jsxs(o,{children:[t.jsx(e,{title:"TrafficLight_Red",subtitle:"",colors:{Default:"#E34537",Hover:"#C92617",Border_BKG:"#F5C7C7"}}),t.jsx(e,{title:"TrafficLight_Yellow",subtitle:"",colors:{Default:"#F7CC57",Hover:"#E2AD1D"}}),t.jsx(e,{title:"TrafficLight_Green",subtitle:"",colors:{Default:"#41CE77",Hover:"#09BD4E",Border_BKG:"#C3F2D7"}})]}),`
+`,t.jsx("br",{}),`
+`,t.jsx(r.h2,{id:"graph-chart-colours",children:"Graph, Chart Colours"}),`
+`,t.jsx("br",{}),`
+`,t.jsxs(o,{children:[t.jsx(e,{title:"Default",subtitle:"Primary Step Switch - Low to High",colors:{1:"#BCE2C7",2:"#4EBCD5",3:"#1A2281"}}),t.jsx(e,{title:"Hover",subtitle:"Primary Step Switch - Low to High",colors:{1:"#94CCA5",2:"#40A2B9",3:"#10155A"}})]}),`
+`,t.jsx("br",{}),`
+`,t.jsxs(o,{children:[t.jsx(e,{title:"Default",subtitle:"Primary Step Switch 2 - Non-hierachial",colors:{1:"#7B3AAD",2:"#C25FD3",3:"#EB709A",4:"#C0ADF2",5:"#D3D1D9"}}),t.jsx(e,{title:"Hover",subtitle:"Primary Step Switch 2 - Non-hierachial",colors:{1:"#592A7E",2:"#9521AA",3:"#CC3E6E",4:"#9D7FEE",5:"#ACA6BC"}})]})]})}function A(s={}){const{wrapper:r}={...l(),...s.components};return r?t.jsx(r,{...s,children:t.jsx(i,{...s})}):i(s)}export{A as default};
