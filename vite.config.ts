6d0f50dae854aca839ab31d498d650e83cf495fa
raoasifraza11 <raoasifraza1@hotmail.com>
2024-12-11 19:00:42 +0500

    fix: tenstack remvoed

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@icons': path.resolve(__dirname, 'src/assets/icons')
