257f1009d38fe592a16bf028d6e64fbc752af8c6
raoasifraza11 <raoasifraza1@hotmail.com>
2024-12-16 22:45:14 +0500

    fix: colors

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -5,35 +5,7 @@ const vaultraTheme = create({
   base: 'light', // Or 'dark'
   brandTitle: 'Vaultra Design System',
   brandUrl: 'https://primetrader.com',
-  brandImage: './logo.svg',
-
-  // Colors
-  colorPrimary: '#171616',
-  colorSecondary: '#000000',
-
-  // UI
-  appContentBg: '#C3F84D',
-  appBorderColor: '#252424',
-  appBorderRadius: 8,
-
-  // Typography
-  fontBase: '"Inter", sans-serif',
-  fontCode: 'monospace',
-
-  // Text Colors
-  textColor: '#000000',
-  textInverseColor: '#ffffff',
-
-  // Toolbar
-  barTextColor: '#000000',
-  barSelectedColor: '#000000',
-  barBg: '#D4FF75',
-
-  // Input
-  inputBg: '#ffffff', // Input background
-  inputBorder: '#e6e6e6', // Input border
-  inputTextColor: '#000000', // Input text color
-  inputBorderRadius: 4 // Input border radius
+  brandImage: './logo.svg'
 })
 
 addons.setConfig({
